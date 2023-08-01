e024799dda660981c9d8683f39d4c2a464169db0
Evgenii Vlasov <senzik@gmail.com>
2023-08-01 09:18:01 +0300

    -- Исправлено по коментариям ментора

---
+++ src/main/java/org/example/BoundaryService.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class BoundaryService {
+    public int findMin(int[] marks) {
+        int currentMin = marks[0];
+        for (int mark : marks) {
+            if (currentMin > mark) {
+                currentMin = mark;
+            }
+        }
+        return currentMin;
+    }
+}
---
+++ src/test/java/org.example/example/BoundaryServiceTest.java
@@ -0,0 +1,38 @@
+package org.example.example;
+
+import org.example.BoundaryService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoundaryServiceTest {
+    BoundaryService boundaryService = new BoundaryService();
+
+    private static int[][] arrayProvider(){
+        return new int[][]{ {-5, 1, 5, 9 ,58}, {58, 9, 5, 1, -5} , {-1, 0, -57, 100, -99, 1000}};
+    }
+
+    @ParameterizedTest(name = "Корректные значения")
+    @MethodSource("arrayProvider")
+    @Disabled
+    void positiveTest(int[] arr){
+        var result = boundaryService.findMin(arr);
+        var expected = Arrays.stream(arr).min().getAsInt();
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest(name = "Пустой массив")
+    @EmptySource
+    void arrIsEmpty(int[] emptyArr){
+        Throwable thrown = assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            boundaryService.findMin(emptyArr);
+        });
+        assertNull(thrown.getMessage());
+    }
+}
+
